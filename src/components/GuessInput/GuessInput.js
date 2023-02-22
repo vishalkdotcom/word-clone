d9a0e1826003f7a43369ba350e64f27b312ff38a
Vishal Kumar <me@vishalk.com>
2023-02-22 16:00:57 +0530

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(guess);
+    setGuess('');
+  };
+
+  return (
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
+      <label htmlFor='guess-input'>Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        id='guess-input'
+        type='text'
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
